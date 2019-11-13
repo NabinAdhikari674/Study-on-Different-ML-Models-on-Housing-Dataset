aed5561f0563af8f13937b43221b9e8b4597a316
Nabin Adhikari <36256500+NabinAdhikari674@users.noreply.github.com>
2019-11-13 10:30:09 +0530

    Added Random Forest Regressor
    Added RF Regressor as a evaluation tool for further comparisions

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from matplotlib import pyplot as plt
 print("\n\n\tRunning main.py\n")
 
@@ -20,6 +21,8 @@ pr1=LinearRegression()
 pr2=LinearRegression()
 quadratic1=PolynomialFeatures(degree=2)
 quadratic2=PolynomialFeatures(degree=3)
+dtr=DecisionTreeRegressor(max_depth=3)
+rfr = RandomForestRegressor(n_estimators=100)
 
 
 
@@ -29,6 +32,33 @@ Xquad2=quadratic2.fit_transform(Xtrain)
 lr.fit(Xtrain,Ytrain)
 pr1.fit(Xquad1,Ytrain)
 pr2.fit(Xquad2,Ytrain)
+
+
+y_lin_pred=lr.predict(Xtest)
+y_quad1_pred=pr1.predict(quadratic1.fit_transform(Xtest))
+y_quad2_pred=pr2.predict(quadratic2.fit_transform(Xtest))
+print("Training MSE => Linear : %.3f, \n\tQuadratic1(d=2) : %.3f, \n\tQuadratic2(d=3) : %.3f"%(mean_squared_error(Ytest,y_lin_pred),
+                                                                                        mean_squared_error(Ytest,y_quad1_pred),
+                                                                                        mean_squared_error(Ytest,y_quad2_pred)))
+print("Training R2 score => Linear : %.3f, \n\tQuadratic(d=2) : %.3f, \n\tQuadratic2(d=3) : %.3f"%(r2_score(Ytest,y_lin_pred),
+                                                                                      r2_score(Ytest,y_quad1_pred),
+                                                                                      r2_score(Ytest,y_quad2_pred)))
+
+
+dtr.fit(Xtrain,Ytrain)
+dtr_pred=dtr.predict(Xtest)
+print("Training MSE => DTR : %.3f"%(mean_squared_error(Ytest,dtr_pred)))
+print("Training R2 score => DTR : %.3f"%(r2_score(Ytest,dtr_pred)))
+
+rfr.fit(Xtrain,Ytrain)
+rfr_pred=rfr.predict(Xtest)
+print("Training MSE => RFR : %.3f"%(mean_squared_error(Ytest,rfr_pred)))
+print("Training R2 score => RFR : %.3f"%(r2_score(Ytest,rfr_pred)))
+
+#plt.scatter(Xtest,Ytest,label='Testing Points')
+#plt.plot(Xtest,dtr_pred,label='DT Regression',color='red')
+#plt.legend()
+#plt.show()
 
 
 '''
@@ -43,26 +73,5 @@ plt.plot(x_test,y_quad2_fit,label='Quadratic Fit2(d=3)')
 plt.legend(loc='upper left')
 plt.show()
 #'''
-
-y_lin_pred=lr.predict(Xtest)
-y_quad1_pred=pr1.predict(quadratic1.fit_transform(Xtest))
-y_quad2_pred=pr2.predict(quadratic2.fit_transform(Xtest))
-print("Training MSE => Linear : %.3f, \n\tQuadratic1(d=2) : %.3f, \n\tQuadratic2(d=3) : %.3f"%(mean_squared_error(Ytest,y_lin_pred),
-                                                                                        mean_squared_error(Ytest,y_quad1_pred),
-                                                                                        mean_squared_error(Ytest,y_quad2_pred)))
-print("Training R2 score => Linear : %.3f, \n\tQuadratic(d=2) : %.3f, \n\tQuadratic2(d=3) : %.3f"%(r2_score(Ytest,y_lin_pred),
-                                                                                      r2_score(Ytest,y_quad1_pred),
-                                                                                      r2_score(Ytest,y_quad2_pred)))
-
-dtr=DecisionTreeRegressor(max_depth=3)
-dtr.fit(Xtrain,Ytrain)
-dtr_pred=dtr.predict(Xtest)
-print("Training MSE => DTR : %.3f"%(mean_squared_error(Ytest,dtr_pred)))
-print("Training R2 score => DTR : %.3f"%(r2_score(Ytest,dtr_pred)))
-
-#plt.scatter(Xtest,Ytest,label='Testing Points')
-#plt.plot(Xtest,dtr_pred,label='DT Regression',color='red')
-#plt.legend()
-#plt.show()
 
 print("Done")
